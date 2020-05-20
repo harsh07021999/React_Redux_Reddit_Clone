1c4508605cfc025b302b822c12d193c6e1862607
Harsh <harshfebruary@gmail.com>
2020-05-20 23:07:49 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import store from './app/store';
-import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import Root from './containers/Root'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Root />,
   document.getElementById('root')
 );
 
